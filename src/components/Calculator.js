b94806dc5f04c38b5a52024776d66cb27f9476e8
mickmed <mickroth66@gmail.com>
2019-01-09 19:43:18 -0500

    add style

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,36 +5,25 @@ class Calculator extends Component {
     super(props);
     this.state = { 
         value: '',
-        sum: '',
+        eq: '',
         ans: 0
     };
   }
 
   handleChange = (event) => {
-    let sh = event.target.value
-    console.log(event.target.value)
-    // this.setState(prevState => {
-    //     return{
-    //         sum: prevState.sum + sh
-    //     }
-    // })
-    this.setState({sum: event.target.value})
-    
-    // console.log(this.state.sum)
+    this.setState({eq: event.target.value})
     event.preventDefault();
   }
 
   handleSubmit = (event) => {
-   
     event.preventDefault();
   }
 
-
   clkMathEq = event => {
-      let sh = event.target.innerText
+    let sh = event.target.innerText
     this.setState(prevState => {
         return{
-            sum: prevState.sum + sh
+            sum: prevState.eq + sh
         }
     })
   
@@ -42,15 +31,25 @@ class Calculator extends Component {
   }
 
   clkEquals = event => {
-      let a = eval(this.state.sum)
+      let a = eval(this.state.eq)
       console.log(a)
 
-      this.setState(prevState => {
-          return{
-              ans:a
-          }
-      })
+      this.setState({ans:a})
+              
+          
       event.preventDefault()
+  }
+
+  handleKeyUp = event => {
+    let a = eval(this.state.eq)
+    console.log(event.keyCode)
+
+    this.setState(prevState => {
+        return{
+            ans:a
+        }
+    })
+    event.preventDefault()
   }
 
 
@@ -59,40 +58,51 @@ class Calculator extends Component {
       
     return (
       <div className="container">
-        <h1>MM</h1>
+      <div className="inner-cont">
+        <h1><i>React Math Master</i></h1>
         <form onSubmit={this.handleSubmit}>
-          
-            <input
-              type="text"
-              value={this.state.sum}
-              onChange={this.handleChange}
-            
-            />
-            <input
-              type="text"
-              value={this.state.ans}
-              onChange={this.handleChange}
-            
-            />
-            <button type="button"  onClick={this.clkMathEq}>1</button>
-            <button type="button"  onClick={this.clkMathEq}>2</button>
-            <button type="button"  onClick={this.clkMathEq}>3</button>
-            <button type="button"  onClick={this.clkMathEq}>4</button>
-            <button type="button"  onClick={this.clkMathEq}>5</button>
-            <button type="button"  onClick={this.clkMathEq}>6</button>
-            <button type="button"  onClick={this.clkMathEq}>7</button>
-            <button type="button"  onClick={this.clkMathEq}>8</button>
-            <button type="button"  onClick={this.clkMathEq}>9</button>
-            <button type="button"  onClick={this.clkMathEq}>0</button>
-            <button type="button"  onClick={this.clkMathEq}>+</button>
-            <button type="button"  onClick={this.clkMathEq}>-</button>
-            <button type="button"  onClick={this.clkMathEq}>*</button>
-            <button type="button"  onClick={this.clkMathEq}>/</button>
-            <button type="button"  onClick={this.clkEquals}>=</button>
-
+            <div className="inputs">
+                <input
+                type="text"
+                value={this.state.eq}
+                onChange={this.handleChange}
+                onKeyUp={this.handleKeyUp}
+                autoFocus
+                
+                />
+                <span className="equalsSign">=</span>
+                <input
+                type="text"
+                value={this.state.ans}
+                onChange={this.handleChange}
+                
+                
+                />
+            </div> 
+            <div>
+                <button type="button"  onClick={this.clkMathEq}>1</button>
+                <button type="button"  onClick={this.clkMathEq}>2</button>
+                <button type="button"  onClick={this.clkMathEq}>3</button>
+                <button type="button"  onClick={this.clkMathEq}>4</button>
+                <button type="button"  onClick={this.clkMathEq}>5</button>
+                <button type="button"  onClick={this.clkMathEq}>6</button>
+                <button type="button"  onClick={this.clkMathEq}>7</button>
+                <button type="button"  onClick={this.clkMathEq}>8</button>
+                <button type="button"  onClick={this.clkMathEq}>9</button>
+                <button type="button"  onClick={this.clkMathEq}>0</button>
+                
+            </div>
+            <div>
+                <button type="button"  onClick={this.clkMathEq}>+</button>
+                <button type="button"  onClick={this.clkMathEq}>-</button>
+                <button type="button"  onClick={this.clkMathEq}>*</button>
+                <button type="button"  onClick={this.clkMathEq}>/</button>
+                <button type="button"  onClick={this.clkEquals}>=</button>
+            </div>
 
           {/* <input type="submit" value="Submit" /> */}
         </form>
+        </div>
       </div>
     );
   }
